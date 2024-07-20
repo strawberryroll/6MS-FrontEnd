e0f8dc472d06d36276985f9662ec7a7788277d8e
ahyeon <nuyha234@gmail.com>
2024-07-20 13:42:11 +0900

    feat: mainPage 세팅

---
+++ service/src/pages/mainPage/main.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function MainPage() {
+    return (
+        <div>
+            <h1>Main Page</h1>
+            <p>This is the main page of your application.</p>
+        </div>
+    );
+}
+
+export default MainPage;
