5063a75c33b88934b6385c2a69c258953b1e45c2
swonlee615 <susanny@naver.com>
2024-08-04 04:27:03 +0900

    Fix:merge를 위한 전체 파일 커밋

--- service/src/App.js
+++ service/src/App.js
@@ -3,12 +3,13 @@ import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import HomePage from './pages/homePage/home';
 import MemberPage from './pages/memberPage/member';
-import MyPage from './pages/myPage/my';
+import MyPage from './pages/myPage/mypage';
 import QuestionPage from './pages/questionPage/question';
 import RecipePage from './pages/recipePage/recipe';
 import StartPage from './pages/startPage/start';
 import SignUp from './pages/memberPage/signup';
 import Search from './pages/search/search';
+import SearchResult from './pages/search/search_result';
 
 function App() {
   return (
@@ -19,9 +20,10 @@ function App() {
         <Route path='/question' element={<QuestionPage />}></Route>
         <Route path='/home' element={<HomePage />}></Route>
         <Route path='/recipe' element={<RecipePage />}></Route>
-        <Route path='/my' element={<MyPage />}></Route>
+        <Route path='/mypage' element={<MyPage />}></Route>
         <Route path='/member/signup' element={<SignUp />}></Route>
         <Route path='/search' element={<Search />}></Route>
+        <Route path='search/search_result' element={<SearchResult />}></Route>
       </Routes>
     </div>
    
